6dd4c9eed91dc32fbadb88e66be4986f55eb2199
Gabriel Krupek <SEU_EMAIL@gmail.com>
2026-09-13 17:52:21 -0300

    ajustes definitivos no web

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,3 @@ Route::resource('categorias', CategoriaController::class)->except('show');
 Route::resource('locais', LocalController::class)->except('show');
 Route::resource('itens', ItemController::class);
 Route::resource('movimentacoes', MovimentacaoController::class)->only(['index', 'create', 'store', 'destroy']);
-
