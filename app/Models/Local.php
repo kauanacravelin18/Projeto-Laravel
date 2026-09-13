ed41e98d1f3cb7874834e2182ab15b6411784393
KauaDLG <engs-kauadelgado@camporeal.edu.br>
2026-09-13 15:43:43 -0300

    criando o model local

---
+++ app/Models/Local.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Local extends Model
+{
+    use HasFactory;
+
+    protected $table = 'locais';
+
+    protected $fillable = ['nome', 'descricao'];
+
+    public function itens(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
+}
